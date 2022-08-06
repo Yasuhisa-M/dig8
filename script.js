26b1a112e99713f33e483b458f4cb3114ddc27ad
Yasuhisa-M <41286820+Yasuhisa-M@users.noreply.github.com>
2022-08-06 22:54:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function animateBlocks() {
     anime({
         targets: ".block",
         translateX: function() {
-            return anime.random(-400, 400);
+            return anime.random(-500, 500);
         },
         translateY: function() {
             return anime.random(-650, 30);
